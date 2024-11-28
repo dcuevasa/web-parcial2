62b405e26c993f2b84fcd61d4b02af536b66e278
dcuevasa <d.cuevas@uniandes.edu.co>
2024-11-28 10:47:54 -0500

    implemented controller

--- src/bono/bono.controller.ts
+++ src/bono/bono.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { BonoService } from './bono.service';
 
 @Controller('bono')
-export class BonoController {}
+export class BonoController {
+    constructor(private readonly bonoService: BonoService) {
+        
+    }
+}
--- src/bono/bono.dto/bono.dto.ts
+++ src/bono/bono.dto/bono.dto.ts
@@ -1,1 +1,15 @@
-export class BonoDto {}
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from "class-validator";
+
+export class BonoDto {
+    @IsNotEmpty()
+    @IsString()
+    nombre: string;
+    
+    @IsNotEmpty()
+    @IsNumber()
+    precio: number;
+    
+    @IsNotEmpty()
+    @IsUrl()
+    imagenUrl: string;
+}
